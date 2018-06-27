0faef12be9c7243e85288b90d49e4a98a7960d03
Michael Parton <mikeparton@live.com.au>
2018-06-28 09:31:35 +1000

    Add mike and coin characters

--- js/components/Character/characters.js
+++ js/components/Character/characters.js
@@ -1,10 +1,18 @@
 const characters = {
   astroman: {
-    model: require('./res/astroman.obj'),
-    material: require('./res/astroman.mtl'),
+    model: require('./astroman/astroman.obj'),
+    material: require('./astroman/astroman.mtl'),
     offset: [0, -5, 0],
     rotationOffset: [270, 0, 0],
     initialScale: [.05, .05, .05]
+  },
+  mike: {
+    model: require('./mike/mike.obj'),
+    material: require('./mike/mike.mtl')
+  }
+  coin: {
+    model: require('./coin/coin.obj'),
+    material: require('./coin/coin.mtl')
   }
 };
 
