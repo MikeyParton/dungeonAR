d5bebb24a0cf55fbd3093bfb482a1e727c8ee0fe
Michael Parton <mikeparton@live.com.au>
2018-06-29 10:57:41 +1000

    Swipe gesture

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,21 @@
 export const randomElement = (array) => (
   array[Math.floor(Math.random() * array.length)]
 )
+
+export const sumArrays = (array1, array2) => (
+  array1.map(function(value, index) {
+    return value + array2[index]
+  })
+)
+
+export const subtractArrays = (array1, array2) => (
+  array1.map(function(value, index) {
+    return value - array2[index]
+  })
+)
+
+export const multiplyArrays = (array1, array2) => (
+  array1.map(function(value, index) {
+    return value * array2[index]
+  })
+)
